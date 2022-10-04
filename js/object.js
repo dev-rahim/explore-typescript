a03b499172604a182d47089189af6adb3ca9f1b5
dev-rahim <rahimuddin5915@gmail.com>
2022-10-04 22:52:13 +0600

    exporing typescript object type set mecanijs

---
+++ js/object.js
@@ -0,0 +1,4 @@
+"use strict";
+const city = 'Dhaka';
+const fruits = ['a', 'b', 'c'];
+const student = { name: 'is a good boy', id: 1256, salary: 15990 };
---
+++ object.ts
@@ -0,0 +1,5 @@
+const city: string = 'Dhaka';
+const fruits: string[] = ['a', 'b', 'c'];
+// const student: { name: string, id: number } = { name: 'is a good boy', id: 1256 }
+type studentType = { salary: number, name: string, id: number };
+const student: studentType = { name: 'is a good boy', id: 1256, salary: 15990 }
