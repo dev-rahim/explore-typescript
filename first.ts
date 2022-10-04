37556b0fc8426fbc2eee68dec55fcc5c331d70cd
dev-rahim <rahimuddin5915@gmail.com>
2022-10-04 21:39:14 +0600

    typescript is vary scrict about resign a value with another type of value

---
+++ first.ts
@@ -0,0 +1,15 @@
+// console.log('Hello TypeScript');
+// console.log('Again Hello TypeScript');
+// const n = 12;
+// let age = 52;
+// console.log(n, age);
+
+let StudentName: string = 'Rahim Uddin';
+let StudentAge: number = 22;
+let isPresent: boolean = true;
+
+// when reassign the value cannot change the valueType
+
+// StudentAge=''; error
+// StudentName=true; error
+// isPresent='true'; error
---
+++ js/first.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log('Hello TypeScript');
+console.log('Again Hello TypeScript');
+const n = 12;
+let age = 52;
+console.log(n, age);
